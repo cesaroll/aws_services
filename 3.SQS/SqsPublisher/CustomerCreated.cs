9748f04f99bb12fbb5d1f2d28d5e9ce595f27268
Cesar <8007837+cesaroll@users.noreply.github.com>
2023-09-30 01:03:47 +0200

    SQS Publisher

---
+++ 3.SQS/SqsPublisher/CustomerCreated.cs
@@ -0,0 +1,14 @@
+/*
+ * @author: Cesar Lopez
+ * @copyright 2023 - All rights reserved
+ */
+namespace SqsPublisher;
+
+public class CustomerCreated
+{
+    public required Guid Id {get; init;}
+    public required string FullName { get; set; }
+    public required string Email { get; set; }
+    public required string GitHubUsername { get; set; }
+    public required DateTime DateOfBirth { get; set; }
+}
---
+++ 3.SQS/SqsPublisher/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated()
+{
+    Id = Guid.NewGuid(),
+    Email = "cesarlerdeut@gmail.com",
+    FullName = "Cesar Lerdeut",
+    DateOfBirth = new DateTime(1980,1,1),
+    GitHubUsername = "cesaroll"
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendmessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var sendMessageResponse = await sqsClient.SendMessageAsync(sendmessageRequest);
+
+Console.WriteLine($"Message sent with id {sendMessageResponse.MessageId}");
