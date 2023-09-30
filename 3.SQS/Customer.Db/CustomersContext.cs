acfe018b6e6d0eac3bccac85c01271cead42453e
Cesar <8007837+cesaroll@users.noreply.github.com>
2023-09-30 13:22:06 +0200

    Acaffold DB

---
+++ 3.SQS/Customer.Db/CustomersContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Customer.Db;
+
+public partial class CustomersContext : DbContext
+{
+    public CustomersContext()
+    {
+    }
+
+    public CustomersContext(DbContextOptions<CustomersContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Entities.Customer> Customers { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Entities.Customer>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("Customers_pkey");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.DateOfBirth).HasColumnType("timestamp without time zone");
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.FullName).HasMaxLength(100);
+            entity.Property(e => e.GitHubUsername).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ 3.SQS/Customer.Db/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Customer.Db.Entities;
+
+public partial class Customer
+{
+    public Guid Id { get; set; }
+
+    public string FullName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string GitHubUsername { get; set; } = null!;
+
+    public DateTime DateOfBirth { get; set; }
+}
